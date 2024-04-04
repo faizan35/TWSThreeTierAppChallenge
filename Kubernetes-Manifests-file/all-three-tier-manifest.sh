2c62833f62ed36af74e88e8f92d07e0d8cd96862
faizan35 <faizanansari4862@gmail.com>
2024-04-05 02:39:45 +0530

    space added

--- Kubernetes-Manifests-file/all-three-tier-manifest.sh
+++ Kubernetes-Manifests-file/all-three-tier-manifest.sh
@@ -10,8 +10,9 @@ apply_manifests() {
 
 # Create namespace three-tier if it doesn't exist
 if ! kubectl get namespace three-tier &> /dev/null; then
-    echo "Creating namespace three-tier..."
+    echo "."
     kubectl create namespace three-tier
+    echo "."
 fi
 
 sleep 1
@@ -19,30 +20,35 @@ sleep 1
 # Main script
 echo "Starting manifest application process..."
 
+echo "."
 
 # Apply manifest files in Database directory
 apply_manifests "Database"
 
 # Wait for 5 seconds before proceeding to the next directory
-echo "Waiting 5 seconds..."
+echo "."
 sleep 5
 
 # Apply manifest files in Backend directory
 apply_manifests "Backend"
 
 # Wait for 5 seconds before proceeding to the next directory
-echo "Waiting 5 seconds..."
+echo "."
 sleep 5
 
 # Apply manifest files in Frontend directory
 apply_manifests "Frontend"
 
+echo "."
+
 echo "Manifest application process completed."
 
+echo "."
+
 # Get pods in namespace three-tier
-echo "Getting pods in namespace three-tier..."
+echo "kubectl get pods -n three-tier"
 kubectl get pods -n three-tier
 
 # Get svc in namespace three-tier
-echo "Getting Services in namespace three-tier..."
+echo "kubectl get svc -n three-tier"
 kubectl get svc -n three-tier
